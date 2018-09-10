25c98c6af98bd709f88dfbf6afe29048b2ae785c
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-09-10 15:04:28 -0400

    Obtener informacion de usuario logueado

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import * as firebase from 'firebase';
 import Swal from 'sweetalert2';
 
 @Injectable({
@@ -11,12 +12,18 @@ export class AuthService {
   constructor(private afAuth: AngularFireAuth,
     private router: Router) { }
 
+    initAuthListener() {
+      this.afAuth.authState.subscribe((fbUser: firebase.User) => {
+        console.log(fbUser);
+      });
+    }
+
   crearUsuario(nombre: string, email: string, password: string) {
     this.afAuth
     .auth
     .createUserWithEmailAndPassword(email, password)
     .then(respuesta => {
-      console.log(respuesta);
+      // console.log(respuesta);
       this.router.navigate(['/']);
     })
     .catch(error => {
@@ -30,7 +37,7 @@ export class AuthService {
     this.afAuth.auth
     .signInWithEmailAndPassword(email, password)
     .then(res => {
-      console.log(res);
+      // console.log(res);
       this.router.navigate(['/']);
     })
     .catch(err => {
