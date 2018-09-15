a06b080e3f01ddd929db48ca4e01cfe56b712790
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-09-15 17:34:42 -0400

    Mostrar todos los datos de ingreso-egreso

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AppState } from '../../app.reducer';
+import { Store } from '@ngrx/store';
+import { IngresoEgreso } from '../ingreso-egreso.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styles: []
 })
-export class DetalleComponent implements OnInit {
+export class DetalleComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  items: IngresoEgreso[];
+  subsIn: Subscription = new Subscription();
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subsIn = this.store.select('ingresoEgreso')
+    .subscribe(
+      ingre => {
+        // console.log(ingre.items);
+        this.items = ingre.items;
+      }
+    );
+  }
+  ngOnDestroy() {
+    this.subsIn.unsubscribe();
+  }
+  borrarItem(uid: string) {
+    console.log(uid);
   }
 
 }
