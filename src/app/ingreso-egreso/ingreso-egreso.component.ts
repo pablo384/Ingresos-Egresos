15b0bfc40ad81f860fa5391facd60691b8a9b9c6
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-09-15 16:24:00 -0400

    optimizando loading y no grabar uid

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,25 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IngresoEgreso } from './ingreso-egreso.model';
 import { IngresoEgresoService } from './ingreso-egreso.service';
 import Swal from 'sweetalert2';
+import { AppState } from '../app.reducer';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.accions';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
   styles: []
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
   tipo = 'ingreso';
+  subscription: Subscription;
+  cargando: boolean;
 
   constructor(
-    public ingresoEgresoService: IngresoEgresoService
+    public ingresoEgresoService: IngresoEgresoService,
+    public store: Store<AppState>
   ) { }
 
   ngOnInit() {
     this.crearForm();
+    this.subscription = this.store.select('ui').subscribe(
+      ui => {
+        this.cargando = ui.isLoading;
+      }
+    );
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
   crearForm() {
     this.form = new FormGroup({
@@ -29,6 +44,7 @@ export class IngresoEgresoComponent implements OnInit {
   }
 
   crearIngresoEgreso() {
+    this.store.dispatch(new ActivarLoadingAction());
     const ingresoEgreso = new IngresoEgreso({ ...this.form.value, tipo: this.tipo });
     // console.log(ingresoEgreso);
     this.ingresoEgresoService.crearIngresoEgreso(ingresoEgreso)
@@ -37,7 +53,7 @@ export class IngresoEgresoComponent implements OnInit {
         this.form.reset({
           monto: 0
         });
-
+        this.store.dispatch(new DesactivarLoadingAction());
       });
   }
 
--- src/app/ingreso-egreso/ingreso-egreso.model.ts
+++ src/app/ingreso-egreso/ingreso-egreso.model.ts
@@ -8,6 +8,6 @@ export class IngresoEgreso {
         this.descripcion =  obj && obj.descripcion || null;
         this.monto =  obj && obj.monto || null;
         this.tipo =  obj && obj.tipo || null;
-        this.uid =  obj && obj.uid || null;
+        // this.uid =  obj && obj.uid || null;
     }
 }
