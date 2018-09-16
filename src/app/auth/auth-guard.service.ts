a607bc7313d3df2ef47c8b8b0141fe0a8396aa93
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-09-16 12:14:19 -0400

    can load

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, CanLoad } from '@angular/router';
 import { AuthService } from './auth.service';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanLoad {
 
   constructor(private authS: AuthService) { }
 
   canActivate() {
     return this.authS.isAuth();
+  }
+  canLoad() {
+    return this.authS.isAuth()
+    .pipe(
+      take(1)
+    );
   }
 }
