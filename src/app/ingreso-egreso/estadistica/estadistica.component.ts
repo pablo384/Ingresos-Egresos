c0a2b010656fca15a1d1d4edd50986ff9520bffb
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-09-16 10:02:34 -0400

    estadisticas basicas

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { IngresoEgreso } from '../ingreso-egreso.model';
 
 @Component({
   selector: 'app-estadistica',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticaComponent implements OnInit {
 
-  constructor() { }
+  ingresos: number;
+  egresos: number;
+
+  cuantosegresos: number;
+  cuantosingresos: number;
+
+  subscripcion: Subscription = new Subscription();
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subscripcion = this.store.select('ingresoEgreso')
+      .subscribe(
+        ingresoEgeso => {
+          this.contarIngresoEgreso(ingresoEgeso.items);
+        }
+      );
+  }
+  contarIngresoEgreso(items: IngresoEgreso[]) {
+    this.ingresos = 0;
+    this.egresos = 0;
+
+    this.cuantosegresos = 0;
+    this.cuantosingresos = 0;
+
+    items.forEach(
+      item => {
+        if (item.tipo === 'ingreso') {
+          this.cuantosingresos++;
+          this.ingresos += item.monto;
+        } else {
+          this.cuantosegresos++;
+          this.egresos += item.monto;
+        }
+      }
+    );
   }
 
 }
